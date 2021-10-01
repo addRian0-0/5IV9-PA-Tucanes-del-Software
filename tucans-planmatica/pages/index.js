3b383e322690df4a44ef6d860c222a0d715183d9
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2021-10-01 16:48:51 -0500

    En el día más brillante, en la noche más oscura

--- tucans-planmatica/pages/index.js
+++ tucans-planmatica/pages/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Image from 'next/image';
 import MainHead from '../components/MainHead';
 import LayoutIndex from '../components/LayoutIndex';
 import styles from "../styles/Home.module.css";
-
+import manzanilla from "../src/index_plant.png";
 export default function Index() {
   return (
     <div>
@@ -13,10 +14,13 @@ export default function Index() {
           <font size={5} face="Work Sans" color="007200">
           <center><h1>Tucan's PlantMatica</h1></center>
           </font>
-          <div className={styles.container_2}>
-            <div className={styles.container_3}>Texto wiiiiiiii</div>
-          </div>
-            
+          <center>
+            <div className={styles.container_3}>
+              Bienvenido a PlantMatica, el sitio ideal para conocer sobre herbolaria en México.<br/>
+              Haznos saber qué buscas y la información llegará volando.
+              <Image src={manzanilla}/>
+            </div>
+          </center>
           </div>
         </font>
       </LayoutIndex>
