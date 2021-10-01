f449742daf9aa45880e94ef37517b6f5df7bb75f
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2021-10-01 17:50:12 -0500

    Hice modificaciones XDDDDDDDDDDDDDD

--- tucans-planmatica/pages/index.js
+++ tucans-planmatica/pages/index.js
@@ -3,7 +3,11 @@ import Image from 'next/image';
 import MainHead from '../components/MainHead';
 import LayoutIndex from '../components/LayoutIndex';
 import styles from "../styles/Home.module.css";
-import manzanilla from "../src/index_plant.png";
+import ip1 from "../src/index_plant.png";
+import ip2 from "../src/index_plant2.png";
+import ip3 from "../src/index_plant3.png";
+import ip4 from "../src/index_plant4.png";
+import ip5 from "../src/index_plant5.png";
 export default function Index() {
   return (
     <div>
@@ -18,7 +22,11 @@ export default function Index() {
             <div className={styles.container_3}>
               <h3>Bienvenido a PlantMatica, el sitio ideal para conocer sobre herbolaria en México.<br/>
               Haznos saber qué buscas y la información llegará volando.<br/><br/><br/></h3>
-              <Image className={styles.index_img} src={manzanilla} width={'100vw'} height={'100vh'} />
+              
+            <button>
+
+            </button>
+
             </div>
           </center>
           </div>
@@ -27,3 +35,29 @@ export default function Index() {
     </div>
   )
 }
+
+/*
+
+              <center>
+              <div className={styles.container_4}>
+              <table>
+                <td>
+                <Image className={styles.index_img} src={ip1} width={'130vw'} height={'110vh'} />
+                </td>
+                <td>
+                  <Image className={styles.index_img} src={ip2} width={'130vw'} height={'110vh'} /> 
+                </td>
+                <td>
+                <Image className={styles.index_img} src={ip3} width={'130vw'} height={'110vh'} />
+                </td>
+                <td>
+                <Image className={styles.index_img} src={ip4} width={'130vw'} height={'110vh'} />
+                </td>
+                <td>
+                <Image className={styles.index_img} src={ip5} width={'130vw'} height={'110vh'} />
+                </td>
+              </table>
+              </div>
+              </center>
+
+*/
