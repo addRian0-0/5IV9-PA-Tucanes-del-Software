3da03d803a6c8e4899c66b985d40b6c516c50824
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2021-10-02 21:14:29 -0500

    xd

--- tucans-planmatica/components/Lateral.jsx
+++ tucans-planmatica/components/Lateral.jsx
@@ -1,3 +1,16 @@
 import React from 'react';
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import MainHead from '../components/MainHead';
+
+export default function Lateral(){
+    return(
+
+        <div>
+            <MainHead tituloPestana="Menú Principal" />
+            <div className={styles.lateral}>
+                ola
+            </div>
+        </div>
+    )
+}
--- tucans-planmatica/pages/menu.jsx
+++ tucans-planmatica/pages/menu.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainHead from '../components/MainHead';
+import LayoutIndex from '../components/Lateral';
+import Lateral from '../components/Lateral';
 
 export default function Menu(){
 
     return(
         <div>
         <MainHead tituloPestana="Menú Principal" />
-        
+        <Lateral></Lateral>
         </div>
 
     )
