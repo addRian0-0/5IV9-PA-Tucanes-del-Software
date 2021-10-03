7d84e2ec8eb57b0361c3536dc3aec4661146e66b
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2021-10-02 21:56:31 -0500

    nose q hice

--- tucans-planmatica/pages/index.js
+++ tucans-planmatica/pages/index.js
@@ -8,7 +8,33 @@ import styles from "../styles/Home.module.css";
 export default function Index() {
   return (
     <div>
-      
+      <MainHead tituloPestana="Bienvenido a PlantMatica" />
+      <LayoutIndex>
+        <font size={4} face="Work Sans">
+          <div className={styles.container_1}>
+            <font size={6} face="Work Sans" color="007200">
+              <center><h1>Tucan's PlantMatica</h1></center>
+            </font>
+            <center>
+              <div className={styles.container_3}>
+                <h3>Bienvenido a PlantMatica, un sitio ideal para conocer más sobre herbolaria en México.<br />
+                  Haznos saber qué buscas y la información llegará volando.</h3>
+                <center>
+                  <br />
+                  <div className={styles.container_4}>
+                    <Link href="./session/CrearCuenta"><button className={styles.btnSubmit} type="submit">Crear Cuenta</button></Link>
+                    <Link href="./session/IniciarSesion"><button className={styles.btnSubmit} type="submit">Iniciar Sesión</button></Link>
+                  </div>
+                  <br />
+                </center>
+                <h3>
+
+                </h3>
+              </div>
+            </center>
+          </div>
+        </font>
+      </LayoutIndex>
     </div>
   )
 }
--- tucans-planmatica/pages/menu.jsx
+++ tucans-planmatica/pages/menu.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import MainHead from '../components/MainHead';
 import LayoutIndex from '../components/Lateral';
 import Lateral from '../components/Lateral';
-
+import styles from "../styles/Home.module.css";
 export default function Menu(){
 
     return(
         <div>
         <MainHead tituloPestana="Menú Principal" />
-        <Lateral></Lateral>
+        <div className={styles.container_0}>
+        <Lateral/>
+        </div>
         </div>
 
     )
