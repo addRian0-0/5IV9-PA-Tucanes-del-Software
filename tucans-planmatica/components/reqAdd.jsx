287ef3ace0089a935a7b56b97e2e845430c00fc1
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2021-10-28 19:57:35 -0500

    Terminé el editar, quedó perrón

--- tucans-planmatica/components/reqAdd.jsx
+++ tucans-planmatica/components/reqAdd.jsx
@@ -38,55 +38,44 @@ export default function FichaUnica() {
             <Card variant="outlined">
                 <CardContent>
                     <p className={styles.titleficha} >Etiquetas: </p>
-                    <textarea type={'text'} className={styles.txtEtiquetas}>apio, comestible, malestar estomacal, enfermedades respiratorias, diarrea</textarea>
+                    <textarea type={'text'} className={styles.txtEtiquetas}></textarea>
                     <hr className={styles.division} />
                     <p className={styles.textU}>Nombre comun: </p>
-                    <textarea className={styles.txtNombres}>Apio</textarea>
+                    <textarea className={styles.txtNombres}></textarea>
                     <hr className={styles.division} />
                     <p className={styles.textU}>Nombre cientifico: </p>
-                    <textarea className={styles.txtNombres}>Apium graveolens</textarea>
+                    <textarea className={styles.txtNombres}></textarea>
                     <hr className={styles.division} />
                     <p className={styles.textU2}>Origen y distribucion: </p>
-                    <textarea className={styles.txtEtiquetas}>Originaria de Japon, Indonesia, Vietnam.</textarea>
+                    <textarea className={styles.txtEtiquetas}></textarea>
                     <hr className={styles.division} />
                     <p className={styles.textD} >Descripcion: </p>
                     <textarea className={styles.txtBody} >
-                        Hierba bienal o perenne, esparcidamente ramificada, sin pelos.Mide de 30 cm a 1 m de alto.Tiene un tallo estriado longitudinalmente,  sus hojas son, de 3 a 25 cm de largo, compuestas con pocos foliolos (pinnadas), peciolos de base envainante, los foliolos son ovados u obovados, cuneados en la base, de 2 a 6 cm de largo por 1 a 5 cm de ancho, margen profundamente lobado o dentado; las hojas superiores más pequeñas que las inferiores y cortamente pecioladas o subsésiles.
-                        Sus flores son pequeñas, de simetría radial, hermafroditas, blancas; cáliz diminuto, unido al ovario; 5 pétalos libres, ovados, el ápice largo y curvado hacia el centro de la flor; los estambres son 5, alternados con los pétalos; ovario ínfero.
-                        Frutos y semillas: El fruto es un esquizocarpo (es decir un fruto indehiscente formado por un gineceo de dos carpelos unidos que en la madurez se separan) casi circular, de aproximadamente 2 mm de largo, comprimido lateralmente, sin pelos, formado por 2 mericarpios (es decir cada una de las partes separables de un fruto) cada uno con una semilla, cada mericarpio tiene en su parte dorsal 5 costillas levemente aladas.
+                        
                     </textarea>
                     <hr className={styles.division} />
                     <p className={styles.textU2}>Caracteristicas especiales: </p>
-                    <textarea className={styles.txtNombres}> Planta aromática.</textarea>
+                    <textarea className={styles.txtNombres}></textarea>
                     <hr className={styles.division} />
                     <p className={styles.titlefichaU}>Alternativa y complementos a: </p>
                     <textarea className={styles.txtBody}>
-                        Paracetamol está indicado para el tratamiento de los síntomas del dolor leve a moderado.
-                        Albuterol (ProAir HFA; Proventil HFA; Ventolin HFA),  (Xopenex HFA) e ipratropio (Combivent), para enfermedades respiratorias.
+                        
                     </textarea>
                     <hr className={styles.division} />
                     <p className={styles.titlefichaU}>Usos:</p>
                     <br />
-                    <textarea className={styles.txtEtiquetas} >
-                        Raiz cocida o cruda
-                        Aromatica
-                        Tallos y hojas crudos o cocinados
-                    </textarea>
+                    <p className={styles.textU2} >
                         
-                        <textarea className={styles.txtBody} placeholder={'Para agregar un uso, escribelo entre comillas simples y separalas con una coma.'}>'Desordenes ginecologicos (regla atrasada).',
-                        'Curar abscesos en mujeres.',
-                        'Transtornosa digestivos como vomito, empacho, enlechado de ninos.',
-                        'Disminuir la produccion de leche',
-                        'Contra el dolor de rinon, vejiga y retencion de orina.',
-                        'Utilizado para el dolor de cabeza',
-                        'Enfermedades respiratorias.'</textarea>
+                    </p>
+                        
+                        <textarea className={styles.txtBody} placeholder={'Para agregar un uso, escribelo entre comillas simples y separalas con una coma.'}></textarea>
 
                     <hr className={styles.division} />
 
                     <div className={styles.fuentes}>
                         <p>Fuentes: </p>
-                        <textarea className={styles.txtRef} placeholder={'Escribelas entre comillas, separalas por una diagonal, entre los paréntesis agrega un enlace y cita en formato APA.'}>
-                            "Rzedowski, G. C. de y J. Rzedowski, 2001. Flora fanerogámica del Valle de México. 2a ed. Instituto de Ecología y Comisión Nacional para el Conocimiento y Uso de la Biodiversidad. Pátzcuaro, Michoacán, México. ()"/"Otra referencia"
+                        <textarea className={styles.txtRef} placeholder={'Escribelas entre comillas, separalas por una diagonal y entre los paréntesis agrega un enlace.'}>
+                            
                         </textarea>
                     </div>
                     <hr className={styles.division} />
@@ -94,7 +83,7 @@ export default function FichaUnica() {
                 </CardContent>
                 <CardActions>
                     <a href="/fichas/ficha">
-                            <button className={styles.btnLinkFicha} onClick={handleToggle}>Guardar</button>
+                            <button className={styles.btnLinkFicha} onClick={handleToggle}>Enviar</button>
                     </a>
                     <Link href="/fichas/ficha">
                             <button className={styles.btnReporte} >Cancelar</button>
